fccfc13bed3ae2bed2f82228eea323ecfd096fe6
Pasgas99 <pasgas99@hotmail.com>
2019-11-16 14:03:43 +0000

    Fixed security camera position on screen

--- src/MySecurityCamera.js
+++ src/MySecurityCamera.js
@@ -2,17 +2,13 @@ class MySecurityCamera extends CGFobject {
     constructor(scene) {
         super(scene);
         
-        this.rectangle = new MyRectangle(scene, 0, 1, 0, 1);
+        this.rectangle = new MyRectangle(scene, 0.5, 1.0, -1.0, -0.5);
         
         this.rttTexture = new CGFtextureRTT(scene, scene.gl.canvas.width, scene.gl.canvas.height);
 
         this.cameraShader = new CGFshader(scene.gl, "shaders/securityCamera.vert", "shaders/securityCamera.frag");
         
         this.cameraShader.setUniformsValues({ uSampler: 0});
-
-        this.cameraMaterial = new CGFappearance(scene);
-        this.cameraMaterial.setTexture(this.rttTexture);
-        this.cameraMaterial.setTextureWrap('REPEAT', 'REPEAT');
     }
 
     attachFrameBuffer() {
@@ -28,7 +24,6 @@ class MySecurityCamera extends CGFobject {
 
         this.scene.setActiveShader(this.cameraShader);
 
-        this.cameraMaterial.apply();
         this.rectangle.display();
 
         this.scene.setActiveShader(this.scene.defaultShader);
