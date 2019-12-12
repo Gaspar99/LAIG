526e90a1c8bd7cd22b94b0b01cf9dbfeec32e9a5
Pasgas99 <pasgas99@hotmail.com>
2019-12-12 11:18:41 +0000

    Added prolog files

--- Proj3/src/game/MyGameOrchestrator.js
+++ Proj3/src/game/MyGameOrchestrator.js
@@ -10,15 +10,15 @@ class MyGameOrchestrator {
     }
 
     setGameboard(component, width, height) {
-        this.gameboard = new this.gameboard(this.scene, component, width, height);
+        this.gameboard = new MyGameboard(this.scene, component, width, height);
     }
 
     setPlayer1PiecesBoard(component, width, height) {
-        this.player1PiecesBoard = new this.gameboard(this.scene, component, width, height);
+        this.player1PiecesBoard = new MyGameboard(this.scene, component, width, height);
     }
 
     setPlayer2PiecesBoard(component, width, height) {
-        this.player2PiecesBoard = new this.gameboard(this.scene, component, width, height);
+        this.player2PiecesBoard = new MyGameboard(this.scene, component, width, height);
     }
 
 
